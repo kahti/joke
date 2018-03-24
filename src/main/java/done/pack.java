5eddb2ae5cf58ead59b3ac95a6b16dcb56e0e6a9
ranjith <ranjithkumar143341@gmail.com>
2018-03-24 14:52:26 +0530

    first log

---
+++ src/main/java/done/pack.java
@@ -0,0 +1,7 @@
+package done;
+
+public class pack {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/done/test.java
@@ -0,0 +1,14 @@
+package done;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void test() {
+
+	}
+
+}
